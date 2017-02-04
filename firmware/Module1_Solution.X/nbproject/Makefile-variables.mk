e6943b6e5cc09682ef2d9f2f7aa69a71f0da12f1
ibhelmer <ihn@ucn.dk>
2017-02-04 22:32:27 +0100

    First commit

---
+++ firmware/Module1_Solution.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Module1_Solution.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Module1_Solution.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=module1solution.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/module1solution.x.tar
